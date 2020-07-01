ccd5486f6c3a329423c908c090d4084a28eee714
Paul Callaghan <p.madden90@gmail.com>
2020-07-01 20:58:51 +0000

    Added Stripe styling

--- checkout/views.py
+++ checkout/views.py
@@ -10,10 +10,12 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-    order_form=OrderForm()
+    order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H0CYOByJvwZPZN7qLo2qZkojnZdYptAeypbTG4Co2IIgfFQy78qA4O05RNZjzXGSgVmHbYU7Q5xXSTUug7JMtZc00smPM5N6w',
+        'client_secret': 'test',
     }
 
     return render(request, template, context)
